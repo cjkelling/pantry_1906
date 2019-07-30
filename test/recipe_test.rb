14888c65209e20bf44014c71182d79344c789e56
Christopher Kelling <cj.kelling88@gmail.com>
2019-07-30 09:21:36 -0600

    Work on amount_required method

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,7 +5,25 @@ require './lib/recipe'
 
 class RecipeTest < Minitest::Test
   def setup
-cheese = Ingredient.new("Cheese", "C", 100)
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+  end
 
+  def test_attributes
+    assert_equal 'Mac and Cheese', @mac_and_cheese.name
+    assert_equal ({}), @mac_and_cheese.ingredients_required
+  end
+
+  def test_add_ingredient
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+    expected = { @cheese => 2, @mac => 8 }
+    assert_equal expected, @mac_and_cheese.ingredients_required
+  end
+
+  def test_amount_required
+    assert_equal 2, @mac_and_cheese.amount_required(@cheese)
+    assert_equal 8, @mac_and_cheese.amount_required(@mac)
   end
 end
